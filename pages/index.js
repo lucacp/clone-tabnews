42fee5073ee1cc11ff01bf11fd060cfa7c5edcd4
lucas6parnoff <lucas_parnoff@hotmail.com>
2025-05-27 22:08:20 -0300

    mensagem para Home

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home(){
-    return <h1>Nova pagina implementada</h1>
+    return <h1>Todos nós, só podemos dar aquilo que nós temos. Por esse motivo aprenda cada vez mais, a ser uma pessoa melhor!</h1>
 }
 export default Home;
