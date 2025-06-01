5e6c70defecf7b568a76531de3af5d8cc451b5f1
lucas6parnoff <lucas_parnoff@hotmail.com>
2025-06-01 19:50:57 -0300

    adicionado `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um clone do site Tabnews.com.br para treinar nodejs com o curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
-function Home(){
-    return <h1>Todos nós, só podemos dar aquilo que nós temos. Por esse motivo aprenda cada vez mais, a ser uma pessoa melhor!</h1>
+function Home() {
+  return (
+    <h1>
+      Todos nós, só podemos dar aquilo que nós temos. Por esse motivo aprenda
+      cada vez mais, a ser uma pessoa melhor!
+    </h1>
+  );
 }
 export default Home;
